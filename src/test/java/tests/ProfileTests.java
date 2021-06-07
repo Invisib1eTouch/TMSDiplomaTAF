79b795c4c102d3bace1de24580cb7a8e5fa72abd
Andrew <andrewvolodko@Andrews-MacBook-Pro.local>
2021-06-07 18:20:54 +0300

    ProfileHeader and ProfileMainTab files added. Test file ProfileTests added

---
+++ src/test/java/tests/ProfileTests.java
@@ -0,0 +1,10 @@
+package tests;
+
+import templates.BaseTestAfterClassDriverDisposing;
+
+public class ProfileTests extends BaseTestAfterClassDriverDisposing {
+
+    public void changeProfileHeaderBackgroundTest(){
+
+    }
+}
