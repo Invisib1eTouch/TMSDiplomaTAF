08f1d1ff2bb0f3553873e50cada0b23b840defa6
Kseniya Tafaniuk <kseniyatafaniuk@Kseniyas-MacBook-Pro.local>
2021-06-12 20:22:23 +0300

    Added locator to SearchResultsFrame

--- src/main/java/pages/SearchResultsFrame.java
+++ src/main/java/pages/SearchResultsFrame.java
@@ -11,6 +11,7 @@ public class SearchResultsFrame extends BasePage {
     private static final By searchResultsIFrameBy = By.className("modal-iframe");
     private static final By searchInputBy = By.className("search__input");
     private static final By searchLoaderBy = By.className("search__bar_searching");
+    private static final By searchResultItemBy = By.className("search__result");
 
     public SearchResultsFrame() {
         super(UrlPrefix.DEFAULT, null);
@@ -27,5 +28,9 @@ public class SearchResultsFrame extends BasePage {
 
     public SelenideElement getSearchLoader() {
         return $(searchLoaderBy);
+    }
+
+    public SelenideElement getSearchResultItem() {
+        return $(searchResultItemBy);
     }
 }
