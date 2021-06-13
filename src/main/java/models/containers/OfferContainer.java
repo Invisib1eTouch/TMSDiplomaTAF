ca4c1177afca0ace13dd85c8c58278b90528cdbe
Kseniya Tafaniuk <kseniyatafaniuk@Kseniyas-MacBook-Pro.local>
2021-06-13 15:18:11 +0300

    Added getMatchedText() to Utils, added fetFloatPrice() to OfferContainer

--- src/main/java/models/containers/OfferContainer.java
+++ src/main/java/models/containers/OfferContainer.java
@@ -3,12 +3,16 @@ package models.containers;
 import com.codeborne.selenide.SelenideElement;
 import lombok.Data;
 
+import static utils.Utils.getMatchedText;
+
 @Data
 public class OfferContainer {
     private final SelenideElement price;
     private final SelenideElement addToCartBtn;
 
     public float getFloatPrice(){
-        return Float.parseFloat(this.price.getOwnText().split(" ")[0]);
+        var pr = this.price.getOwnText();
+        return Float.parseFloat(getMatchedText(this.price.getOwnText(),"(?<=.*)\\d+,\\d{2}(?=.*)")
+                .replace(',', '.'));
     }
 }
