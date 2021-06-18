c09c5fae6537245acb5fbafb78d5155278754aee
Andrew <andrewvolodko@Andrews-MacBook-Pro.local>
2021-06-18 14:33:08 +0300

    parrallel run setup in pr

--- src/main/java/core/DriverClient.java
+++ src/main/java/core/DriverClient.java
@@ -2,6 +2,7 @@ package core;
 
 
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * Setup selenide config
@@ -9,7 +10,18 @@ import com.codeborne.selenide.Configuration;
 public class DriverClient {
 
     public static void init() {
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.driverManagerEnabled = false;
         Configuration.browser = PropertyReader.getBrowserName();
+        Configuration.timeout = 10000;
+        Configuration.pageLoadTimeout = 60000;
         Configuration.startMaximized = true;
+        Configuration.browserCapabilities = new DesiredCapabilities();
+        Configuration.browserCapabilities.setCapability("browserName", PropertyReader.getBrowserName());
+        Configuration.browserCapabilities.setCapability("enableVNC", true);
+        Configuration.browserCapabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities.setCapability("sessionTimeout", "2m");
+        Configuration.browserCapabilities.setCapability("--no-sandbox", true);
+        Configuration.browserCapabilities.setCapability("--disable-gpu", true);
     }
 }
