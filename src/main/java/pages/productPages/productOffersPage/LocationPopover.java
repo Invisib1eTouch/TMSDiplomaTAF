c7dadd171b3cca8659c86f7b3ded0564f47e8446
Aliaksandr Tkachou <aliaksandr.tkachov@gmail.com>
2021-06-18 20:18:07 +0300

    PopupTest refactoring

--- src/main/java/pages/productPages/productOffersPage/LocationPopover.java
+++ src/main/java/pages/productPages/productOffersPage/LocationPopover.java
@@ -29,7 +29,7 @@ public class LocationPopover extends BasePage {
         return $(byXpath(popupContainerLocator + yesBtnLocator));
     }
 
-    public SelenideElement getPopoverContainer(){
+    public SelenideElement getPopupContainer(){
         return $(byXpath(popupContainerLocator));
     }
 }
