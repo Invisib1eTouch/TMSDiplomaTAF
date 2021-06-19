317be36072474a7cc5414e595bf94da3ed3c98e3
kseniyatafaniuk <78101146+kseniyatafaniuk@users.noreply.github.com>
2021-06-20 00:07:11 +0300

    Profile and Search Api tests impl (#21)
    * Added method getSearchResultNameByName to CatalogApiSteps, added tests to SearchApiTests and ProfileApiTests
    
    * Refactored ProfileApiTests and SearchApiTests, updated testng.xml
    
    Co-authored-by: Kseniya Tafaniuk <kseniyatafaniuk@Kseniyas-MacBook-Pro.local>

---
+++ src/test/java/tests/apiTests/SearchApiTests.java
@@ -0,0 +1,23 @@
+package tests.apiTests;
+
+import apiSteps.CatalogApiSteps;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.Utils;
+
+public class SearchApiTests {
+
+    @Test
+    public void searchResultsTest() {
+        var products = CatalogApiSteps.getAvailableMobilePhones().getProducts();
+        var product = products.get(Utils.getRandomNumber(0, products.size()));
+        String extendedProductName = product.getExtendedName();
+
+        var searchResultsResponse = CatalogApiSteps.getSearchResultByProductName(extendedProductName);
+        Assert.assertTrue(searchResultsResponse
+                .getBody()
+                .jsonPath()
+                .getList("products.extended_name")
+                .contains(extendedProductName));
+    }
+}
