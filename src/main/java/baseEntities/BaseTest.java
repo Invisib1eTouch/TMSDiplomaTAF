a7f665def29b5bb51891eeb0ef360fa9718a51be
Andrew <andrewvolodko@Andrews-MacBook-Pro.local>
2021-06-21 21:28:34 +0300

    AllureUtilities removed

--- src/main/java/baseEntities/BaseTest.java
+++ src/main/java/baseEntities/BaseTest.java
@@ -2,9 +2,7 @@ package baseEntities;
 
 import com.codeborne.selenide.SelenideElement;
 import core.DriverClient;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import utils.AllureUtilities;
 
 import java.util.Objects;
 
@@ -16,11 +14,6 @@ public abstract class BaseTest {
     @BeforeSuite
     public void driverSetup(){
         DriverClient.getInstance().setup();
-    }
-
-    @AfterSuite
-    public void suiteTearDown(){
-        AllureUtilities.removeParametersInReport();
     }
 
     protected int getElementTextLength(SelenideElement selenideElement) {
